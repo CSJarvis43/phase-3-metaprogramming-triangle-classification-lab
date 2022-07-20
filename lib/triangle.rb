091866514b3afb3cf7e235034f437d8d296fcf71
Charles Jarvis <csjarvis43@gmail.com>
2022-07-20 16:09:25 -0600

    commit tree 366141a6afc44df133cdce32e80600f73e473e6d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def greater_than_zero
+    a > 0 && b > 0 && c > 0
+  end
+
+  def triangle_inequality
+    a + b > c && b + c > a && a + c > b
+  end
+
+  def raise_error
+    raise TriangleError unless greater_than_zero && triangle_inequality
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
